5f7c1e2eacb5c22e482434fd50a0657d2806fe5d
Raghvendra Tripathi <tripathiraghvendra14@gmail.com>
2024-04-06 09:34:34 +0530

    first commit

---
+++ lib/landing.dart
@@ -0,0 +1,63 @@
+// Home screen that displays the logo and a button to start the session
+
+import 'package:flutter/material.dart';
+
+import 'homepage.dart';
+
+class LandingPage extends StatelessWidget {
+  static const _textStyle = TextStyle(fontFamily: 'Arial');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          // The main content of the page
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const AnimatedOpacity(
+                    duration: Duration(milliseconds: 500),
+                    opacity: 1,
+                    child: Text(
+                      'Welcome to Session',
+                      style: TextStyle(fontSize: 32, color: Colors.black, fontFamily: 'Arial'),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  AnimatedOpacity(
+                    duration: Duration(milliseconds: 500),
+                    opacity: 1,
+                    child: TextButton(
+                      onPressed: () => Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          transitionDuration: Duration(seconds: 1),
+                          pageBuilder: (context, animation, secondaryAnimation) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: HomePage(),
+                            );
+                          },
+                        ),
+                      ),
+                      child: const Text(
+                        'start',
+                        style: TextStyle(color: Colors.greenAccent),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // The footer with the "Made with love in YYC" text
+
+        ],
+      ),
+    );
+  }
+}
